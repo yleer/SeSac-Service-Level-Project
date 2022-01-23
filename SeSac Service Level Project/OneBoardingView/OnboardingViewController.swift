b62df473378640c2c5988394b7e925d1db808905
yleer <spsyoudong1@naver.com>
2022-01-23 17:54:22 +0900

    On Boarding View done.

--- SeSac Service Level Project/OneBoardingView/OnboardingViewController.swift
+++ SeSac Service Level Project/OneBoardingView/OnboardingViewController.swift
@@ -20,6 +20,7 @@ class OnboardingViewController: UIViewController {
         super.viewDidLoad()
         addTargets()
         mainView.configureScrollView()
+        mainView.scrollView.delegate = self
     }
     
     override func viewDidLayoutSubviews() {
@@ -29,14 +30,33 @@ class OnboardingViewController: UIViewController {
     
     private func addTargets() {
         mainView.pageControl.addTarget(self, action: #selector(pageControlDidChange), for: .valueChanged)
+        mainView.startButton.addTarget(self, action: #selector(startButtonClicked), for: .touchUpInside)
     }
     
     
     @objc private func pageControlDidChange(_ sender: UIPageControl) {
         let current = sender.currentPage
-        
+        mainView.scrollView.setContentOffset(CGPoint(x: CGFloat(current) * mainView.scrollView.frame.size.width, y: 0), animated: true)
     }
     
-    
-    
+    @objc private func startButtonClicked(_ sender: UIButton) {
+        let current = mainView.pageControl.currentPage
+        if current == 2 {
+            print("need to segue")
+        }else {
+            mainView.scrollView.setContentOffset(CGPoint(x: CGFloat(current + 1) * mainView.scrollView.frame.size.width, y: 0), animated: true)
+        }
+    }
+}
+
+
+extension OnboardingViewController: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        if scrollView.frame.size.width == 0 {
+            mainView.pageControl.currentPage = Int(floorf(Float(scrollView.contentOffset.x) / Float(1)))
+        }else{
+            mainView.pageControl.currentPage = Int(floorf(Float(scrollView.contentOffset.x) / Float(mainView.scrollView.frame.size.width )))
+        }
+        
+    }
 }
